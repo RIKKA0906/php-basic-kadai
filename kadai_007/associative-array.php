accb37318107e3657109f6a83f02835b60785ffa
RIKKA0906 <peace1008.kikyou@gmail.com>
2025-06-04 07:02:06 +0900

    課題をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <?php 
+    $onion_status = ['name' => 'onion','price' => '200', 'weight' => '160'];
+    print_r($onion_status);
+    ?>
+    
+</body>
+</html>
